f8e846f43c7e92535166e7a609ed3fb0c122fb4a
Ryan <cungdinhuy@gmail.com>
2020-06-09 12:58:13 +0700

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Uy Cung Dinh on 6/9/20.
+//  Copyright © 2020 Daylighter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
