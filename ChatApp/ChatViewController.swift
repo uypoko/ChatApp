2b0bb4899755facbff812a16f42f3d60f6bc5bd7
Ryan <cungdinhuy@gmail.com>
2020-06-09 13:29:49 +0700

    Initial setup

---
+++ ChatApp/ChatViewController.swift
@@ -0,0 +1,216 @@
+//
+//  ChatViewController.swift
+//  SocketChat
+//
+//  Created by Gabriel Theodoropoulos on 1/31/16.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextViewDelegate, UIGestureRecognizerDelegate {
+    
+
+    @IBOutlet weak var tblChat: UITableView!
+    
+    @IBOutlet weak var lblOtherUserActivityStatus: UILabel!
+    
+    @IBOutlet weak var tvMessageEditor: UITextView!
+    
+    @IBOutlet weak var conBottomEditor: NSLayoutConstraint!
+    
+    @IBOutlet weak var lblNewsBanner: UILabel!
+    
+    
+    
+    var nickname: String!
+    
+    var chatMessages = [[String: AnyObject]]()
+    
+    var bannerLabelTimer: Timer!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardDidShowNotification), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardDidHideNotification), name: UIResponder.keyboardDidHideNotification, object: nil)
+        
+        
+        let swipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        swipeGestureRecognizer.direction = UISwipeGestureRecognizer.Direction.down
+        swipeGestureRecognizer.delegate = self
+        view.addGestureRecognizer(swipeGestureRecognizer)
+    }
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        configureTableView()
+        configureNewsBannerLabel()
+        configureOtherUserActivityLabel()
+        
+        tvMessageEditor.delegate = self
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+    // MARK: IBAction Methods
+    
+    @IBAction func sendMessage(sender: AnyObject) {
+
+    }
+
+    
+    // MARK: Custom Methods
+    
+    func configureTableView() {
+        tblChat.delegate = self
+        tblChat.dataSource = self
+        tblChat.register(UINib(nibName: "ChatCell", bundle: nil), forCellReuseIdentifier: "idCellChat")
+        tblChat.estimatedRowHeight = 90.0
+        tblChat.rowHeight = UITableView.automaticDimension
+        tblChat.tableFooterView = UIView(frame: .zero)
+    }
+    
+    
+    func configureNewsBannerLabel() {
+        lblNewsBanner.layer.cornerRadius = 15.0
+        lblNewsBanner.clipsToBounds = true
+        lblNewsBanner.alpha = 0.0
+    }
+    
+    
+    func configureOtherUserActivityLabel() {
+        lblOtherUserActivityStatus.isHidden = true
+        lblOtherUserActivityStatus.text = ""
+    }
+    
+    
+    @objc func handleKeyboardDidShowNotification(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            if let keyboardFrame = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+                conBottomEditor.constant = keyboardFrame.size.height
+                view.layoutIfNeeded()
+            }
+        }
+    }
+    
+    
+    @objc func handleKeyboardDidHideNotification(notification: NSNotification) {
+        conBottomEditor.constant = 0
+        view.layoutIfNeeded()
+    }
+    
+    
+    func scrollToBottom() {
+        let delay = 0.1 * Double(NSEC_PER_SEC)
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime(uptimeNanoseconds: UInt64(delay))) {
+            if self.chatMessages.count > 0 {
+                let lastRowIndexPath = NSIndexPath(row: self.chatMessages.count - 1, section: 0)
+                self.tblChat.scrollToRow(at: lastRowIndexPath as IndexPath, at: UITableView.ScrollPosition.bottom, animated: true)
+            }
+        }
+//
+//        dispatch_after(dispatch_time(dispatch_time_t(DispatchTime.now()), Int64(delay)), DispatchQueue.main) { () -> Void in
+//
+//        }
+    }
+    
+    
+    func showBannerLabelAnimated() {
+        UIView.animate(withDuration: 0.75, animations: { () -> Void in
+            self.lblNewsBanner.alpha = 1.0
+            
+            }) { (finished) -> Void in
+                self.bannerLabelTimer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: Selector(("hideBannerLabel")), userInfo: nil, repeats: false)
+        }
+    }
+    
+    
+    func hideBannerLabel() {
+        if bannerLabelTimer != nil {
+            bannerLabelTimer.invalidate()
+            bannerLabelTimer = nil
+        }
+        
+        UIView.animate(withDuration: 0.75, animations: { () -> Void in
+            self.lblNewsBanner.alpha = 0.0
+            
+            }) { (finished) -> Void in
+        }
+    }
+
+    
+    
+    func dismissKeyboard() {
+        if tvMessageEditor.isFirstResponder {
+            tvMessageEditor.resignFirstResponder()
+        }
+    }
+    
+    
+    
+    // MARK: UITableView Delegate and Datasource Methods
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chatMessages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "idCellChat", for: indexPath as IndexPath) as! ChatCell
+        
+        return cell
+    }
+    
+    // MARK: UITextViewDelegate Methods
+    
+    func textViewShouldBeginEditing(textView: UITextView) -> Bool {
+        return true
+    }
+
+    
+    // MARK: UIGestureRecognizerDelegate Methods
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+}
---
+++ ChatApp/UsersViewController.swift
@@ -0,0 +1,114 @@
+//
+//  UsersViewController.swift
+//  SocketChat
+//
+//  Created by Gabriel Theodoropoulos on 1/31/16.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+
+    @IBOutlet weak var tblUserList: UITableView!
+    
+    
+    var users = [[String: AnyObject]]()
+    
+    var nickname: String!
+    
+    var configurationOK = false
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if !configurationOK {
+            configureNavigationBar()
+            configureTableView()
+            configurationOK = true
+        }
+        
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            if identifier == "idSegueJoinChat" {
+                let chatViewController = segue.destination as! ChatViewController
+                chatViewController.nickname = nickname
+            }
+        }
+    }
+
+    
+    // MARK: IBAction Methods
+    
+    @IBAction func exitChat(sender: AnyObject) {
+        
+    }
+
+    
+    
+    // MARK: Custom Methods
+    
+    func configureNavigationBar() {
+        navigationItem.title = "SocketChat"
+    }
+    
+    
+    func configureTableView() {
+        tblUserList.delegate = self
+        tblUserList.dataSource = self
+        tblUserList.register(UINib(nibName: "UserCell", bundle: nil), forCellReuseIdentifier: "idCellUser")
+        tblUserList.isHidden = true
+        tblUserList.tableFooterView = UIView(frame: .zero)
+    }
+    
+    
+    // MARK: UITableView Delegate and Datasource methods
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "idCellUser", for: indexPath as IndexPath) as! UserCell
+        
+        return cell
+    }
+    
+    private func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 44.0
+    }
+    
+}
